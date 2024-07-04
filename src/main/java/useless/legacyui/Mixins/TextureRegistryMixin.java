15e4a11df955e65e67ad54a48f34a42707d2bd70
WatermelonMojito <104700587+WatermelonMojito@users.noreply.github.com>
2024-07-04 05:23:48 -0500

    fixed icons

--- src/main/java/useless/legacyui/Mixins/TextureRegistryMixin.java
+++ src/main/java/useless/legacyui/Mixins/TextureRegistryMixin.java
@@ -13,7 +13,7 @@ public class TextureRegistryMixin {
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void createIconAtlas(CallbackInfo callbackInfo){
         String[] iconTextures = new String[]{"armor", "bricks", "grass", "health", "lever", "modded", "painting", "planks", "rail", "redstonerail", "tools"};
-        LegacyUI.iconAtlas = TextureRegistry.register("legacyui$icon", new AtlasStitcher("gui/icon", false, "/assets/legacyui/icon/Unknown.png"));
+        LegacyUI.iconAtlas = TextureRegistry.register("legacyui$icon", new AtlasStitcher("icon", true, "/assets/legacyui/icon/Unknown.png"));
         for (String texture: iconTextures) {
             TextureRegistry.getTexture("legacyui:legacyui$icon/" + texture);
         }
